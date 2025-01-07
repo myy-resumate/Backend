140a8d6e8ab87aa945c11f7196f5af94beb46556
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-07 15:45:41 +0900

    프로젝트 생성

---
+++ src/main/java/dev/resumate/ResumateApplication.java
@@ -0,0 +1,13 @@
+package dev.resumate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/resumate/ResumateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.resumate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
