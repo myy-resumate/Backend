c89df917f909f820867368df4d53a576771e369c
Yeonjoo Lee <joos0207@naver.com>
2025-01-13 17:14:25 +0900

    :sparkles: Feat: 엔티티 생성 #1

---
+++ src/main/java/dev/resumate/domain/Member.java
@@ -0,0 +1,27 @@
+package dev.resumate.domain;
+
+import dev.resumate.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
